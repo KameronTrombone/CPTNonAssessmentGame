6817f7a2248e8a8d837f8e33db1b3b305daba3d4
Sebastian <sebastian.kameron@education.nsw.gov.au>
2025-11-20 12:09:30 +1100

    more

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ def main():
     class = int(input("What class would you like to be ? Type '1' for fighter, '2' for ranger, or '3', for wizard" ))
     if class == 1:
         "blah blah blah"
+    elif class == 2:
+        "blah blah blah"
+    elif class == 3:
+        "blah blah blah"
+    elif class != 1, != 2, !=3:
+        print('invalid input')
 
