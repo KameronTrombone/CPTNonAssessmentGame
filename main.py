c44fa05fde2c1968e01f741f1681e6c8d7cd959b
Sebastian <sebastian.kameron@education.nsw.gov.au>
2025-11-20 12:04:28 +1100

    more done to main.py

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ intelligence = 10
 
 def main():
     class = int(input("What class would you like to be ? Type '1' for fighter, '2' for ranger, or '3', for wizard" ))
+    if class == 1:
+        "blah blah blah"
 
