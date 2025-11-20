3e1b7d9755ee4081b73494e434d0d17983258b8c
Sebastian <sebastian.kameron@education.nsw.gov.au>
2025-11-20 15:42:26 +1100

    yada yada

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
+
 health = 10
-strength = 10
-dexterity = 10
-intelligence = 10
+strength = 6
+dexterity = 6
+intelligence = 6
+proffesions = [fighter, ranger, wizard]
+races = [human, orc, elf, gnome]
 
 def main():
-    class = int(input("What class would you like to be ? Type '1' for fighter, '2' for ranger, or '3', for wizard" ))
-    if class == 1:
+    proffesion = proffesions[int(input("What class would you like to be ? Type '0' for fighter, '1' for ranger, or '2', for wizard" ))]
+    if proffesion == fighter:
         "blah blah blah"
-    elif class == 2:
+    elif proffesion == ranger:
         "blah blah blah"
-    elif class == 3:
+    elif proffesion == wizard:
         "blah blah blah"
-    elif class != 1, != 2, !=3:
-        print('invalid input')
+    else:
+        proffesion = fighter
+        print('you are stupid, be a fighter')
+   
+    race = races[int(input("What race would you like to be ? Type '0' for human, '1' for orc, '2' for elf, or '3' for gnome"))]
+    if race == human:
+        strength += 0
+        dexterity -= 0
+        intelligence -= 0
+    elif race == orc:
+        strength += 2
+        dexterity -= 1
+        intelligence -= 1
+    elif race == elf:
+        strength -= 2
+        dexterity += 1
+        intelligence += 1
+    elif race == gnome:
+        strength -=1
+        dexterity += 0
+        intelligence += 2
+        health -= 1
+    print(proffesion, race) 
 
