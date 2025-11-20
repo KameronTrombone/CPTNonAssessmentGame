1badb99500a66a1626068983dfd2b65bba0958b1
Sebastian <sebastian.kameron@education.nsw.gov.au>
2025-11-20 11:56:44 +1100

    Main.py created with some basic setup functions

---
+++ main.py
@@ -0,0 +1,8 @@
+health = 10
+strength = 10
+dexterity = 10
+intelligence = 10
+
+def main():
+    class = int(input("What class would you like to be ? Type '1' for fighter, '2' for ranger, or '3', for wizard" ))
+
