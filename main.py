ff3ecb355a2e8d90adcb63a0e6ff16edab516302
AUTemmie <lucas.chen10@education.nsw.gov.au>
2025-12-01 15:08:20 +1100

    part 1 of roguelike tutorials

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import tcod 
+
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
 health = 10
 strength = 6
 dexterity = 6
@@ -6,7 +11,46 @@ intelligence = 6
 professions = ["fighter", "ranger", "wizard"]
 races = ["human", "orc", "elf", "gnome"]
 
-def main():
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Yet Another Roguelike Tutorial",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+            if action is None:
+                continue
+            if isinstance(action, MovementAction):
+                player_x += action.dx
+                player_y += action.dy
+            elif isinstance(action, EscapeAction):
+                raise SystemExit()
+
     global health, strength, dexterity, intelligence
     
     profession = professions[int(input("Choose class: 0=fighter, 1=ranger, 2=wizard: "))]
@@ -44,4 +88,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-    
+
